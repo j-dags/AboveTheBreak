507bd60f00a8586bf0f2cd99787c4544af029120
j-dags <jon.dagdagan@gmail.com>
2021-02-25 17:01:01 -0600

    responsive

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Api routes
 app.use('/stats', require('./api'));
-// app.use('/rp', require('./api/rp'));
+app.use('/rp', require('./api/rp'));
 
 // Direct other requests to index.html
 // app.get('*', function (req, res) {
