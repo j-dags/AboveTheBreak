391148bd28f93934e6d7f2f369f716d8c7b1d532
j-dags <jon.dagdagan@gmail.com>
2021-03-06 13:12:48 -0600

    fix: league charts

--- src/data/data.js
+++ src/data/data.js
@@ -1,24 +1,24 @@
-// import { data } from './output';
-// import _ from 'lodash';
+import { data } from './dataset_2020-21'
+import _ from 'lodash'
 
-// export const playerData = (arr) => {
-// 	// console.log(data.resultSets[0].headers);
-// 	const headers = arr.resultSets[0].headers;
-// 	let ans = arr.resultSets[0].rowSet;
+export const playerData = (arr) => {
+	// console.log(data.resultSets[0].headers);
+	const headers = arr.resultSets[0].headers
+	let ans = arr.resultSets[0].rowSet
 
-// 	ans = ans.map((player) => {
-// 		return player.reduce((obj, el, idx) => {
-// 			return {
-// 				...obj,
-// 				[_.camelCase(headers[idx])]: el,
-// 			};
-// 		}, {});
-// 	});
-// 	return ans;
-// };
+	ans = ans.map((player) => {
+		return player.reduce((obj, el, idx) => {
+			return {
+				...obj,
+				[_.camelCase(headers[idx])]: el,
+			}
+		}, {})
+	})
+	return ans
+}
 
-// let tableData = playerData(data)
-// 	.filter((player) => player.nbaFantasyPtsRank < 200)
-// 	.sort((a, b) => a.nbaFantasyPtsRank - b.nbaFantasyPtsRank);
+let tableData = playerData(data)
+	.filter((player) => player.nbaFantasyPtsRank < 200)
+	.sort((a, b) => a.nbaFantasyPtsRank - b.nbaFantasyPtsRank)
 
-// export default tableData;
+export default tableData
