bacc691c0493937a34fea3d933bb921b7835d3b9
j-dags <jon.dagdagan@gmail.com>
2021-05-20 17:45:33 -0500

    style: update css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css'
 import App from './components/App'
 import reportWebVitals from './reportWebVitals'
 import { CounterContextProvider } from './components/Context'
-// import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
 	<React.StrictMode>
